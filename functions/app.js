09255b585499c16720ec71958cbd05d2bfa98261
Tokinantenaina <tokib2oba@yahoo.com>
2024-04-28 07:57:53 +0300

    update

--- functions/app.js
+++ functions/app.js
@@ -1,12 +1,24 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const { readjson } = require("./readjson");
+const { readjson } = require("../readjson");
+
 app.get("/.netlify/functions/app", (req, res) => {
   res.json({ mess: "Heloo" });
 });
+
 app.get("/.netlify/functions/app/api", (req, res) => {
   readjson(req, res);
 });
 
 module.exports.handler = serverless(app);
+/*
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.get('*', (req, res) => {
+    res.send("say hello !!!")
+});
+
+module.exports.handler = serverless(app);*/
