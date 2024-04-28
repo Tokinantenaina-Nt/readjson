012be13a8975d8f8772ea0c69e1e4059e7725a53
Tokinantenaina <tokib2oba@yahoo.com>
2024-04-28 10:25:18 +0300

    update

--- readjson.js
+++ readjson.js
@@ -1,15 +1,21 @@
 const fs = require("fs");
 const path = require("path");
-module.exports.readjson = async (req, res) => {
-  const filepath = await path.join(__dirname, "./functions/db.json");
-  fs.access(filepath, fs.constants.F_OK, err => {
-    if (err) {
-      return res.status(404).json({ message: err });
+const db = require("./db.json");
+exports.handler = () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(db),
+    headers: {
+      "Content-Type": "application/json"
     }
-    fs.readFile(filepath, "utf8", (err, data) => {
-      const jsonData = JSON.parse(data);
-      if (err) return res.send({ err });
-      res.json({ message: jsonData.message });
-    });
+  };
+};
+module.exports.readjson = (req, res) => {
+  const filepath = path.join(__dirname, "./db.json");
+
+  fs.readFile(filepath, "utf8", (err, data) => {
+    const jsonData = JSON.parse(data);
+    if (err) return res.send({ err });
+    res.json({ message: jsonData.message });
   });
 };
